ea1272c7b4c874378c7078ce09deefce9f11bb23
dailin <dml4pers4015@gmail.com>
2025-06-15 11:57:36 +0800

    hader添加测试版提醒

--- src/components/TWHeader.vue
+++ src/components/TWHeader.vue
@@ -5,13 +5,20 @@
     class="bg-gradient-to-r from-[#2C75D1] to-[#599DFF] text-white py-4 px-6 fixed top-0 left-0 right-0 z-50 shadow-[0_2px_8px_rgba(0,0,0,0.1)] max-md:px-4 before:content-['']"
   >
     <div class="md:max-w-[50%] w-full flex justify-between items-center mx-auto">
-      <div>
+      <div class="relative">
         <router-link :to="{ name: 'home' }" class="flex gap-1 items-center">
           <div class="h-6 flex items-center justify-center">
             <img src="@/assets/logo.png" alt="Logo" class="object-cover w-16 h-16" />
           </div>
           <h1 class="text-3xl font-bold">dml's notes</h1>
         </router-link>
+        <!-- 添加bate提醒 -->
+        <span
+          class="absolute top-0 right-0 bg-purple-300 text-white text-xs font-bold px-2 py-1 rounded-full -rotate-4"
+          style="transform: translate(50%, -50%)"
+        >
+          BETA
+        </span>
       </div>
 
       <button
