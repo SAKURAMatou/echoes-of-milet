b486b214ee96058eab3bea9b10e68ff42c130499
dl-partner <dailin.partner.mkdf@gmail.com>
2026-07-07 12:13:30 +0900

    main区域的阴影问题

--- src/views/LayoutApp.vue
+++ src/views/LayoutApp.vue
@@ -33,7 +33,7 @@
             "
           >
             <main
-              class="w-full max-w-full rounded-lg border border-white/60 bg-white/60 shadow-[0_24px_90px_-46px_rgba(31,41,55,0.8)] backdrop-blur-xl"
+              class="w-full max-w-full rounded-lg border border-white/60 bg-white/60 shadow-[18px_24px_70px_-52px_rgba(31,41,55,0.55)] backdrop-blur-xl"
               :class="
                 route.meta.widePage
                   ? 'md:max-w-[calc(100vw-380px)] lg:max-w-[calc(100vw-460px)] xl:max-w-[1120px] 2xl:max-w-[1200px]'
